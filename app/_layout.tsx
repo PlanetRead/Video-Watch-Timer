cc0bc88ed13fa34d4e3a20989e8e255b0fabbe12
Jiya Gupta <coderaholic21@gmail.com>
2025-02-18 19:09:20 +0530

    lockOrientation for VideoPlayer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout() {
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
+      <Stack.Screen name="video" options={{ headerShown: false }} />
     </Stack>
     <StatusBar style="auto" />
   </>
