32b96b481ecf34fb16a484238f6553009724daa5
Jiya Gupta <coderaholic21@gmail.com>
2025-03-05 18:33:12 +0530

    Dashboard Analytics

--- app/dashboard/index.tsx
+++ app/dashboard/index.tsx
@@ -1,8 +1,11 @@
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { getVideoAnalyticsByUser,getUsers } from '../database/database';
+import { getVideoAnalyticsByUser, getUsers } from '../database/database';
 import { useSQLiteContext } from 'expo-sqlite';
+import { Dimensions } from 'react-native';
+import { videoDetails } from "../../assets/details";
+import DropDownPicker from 'react-native-dropdown-picker';
 
 // Define type for analytics data
 type AnalyticsData = {
@@ -13,8 +16,15 @@ type AnalyticsData = {
   total_views_day: number;
   date: string;
   last_time_stamp: number | null;
-  user_id?: string; // Mark as optional
+  user_id?: string;
+
+  english_title?: string;
+  punjabi_title?: string;
+  thumbnail_en?: any;
+  thumbnail_punjabi?: any;
+  level?: string;
 };
+
 
 
 const AnalyticsDashboard = () => {
@@ -43,40 +53,150 @@ const AnalyticsDashboard = () => {
     const fetchDetails = async () => {
       try {
         const data: AnalyticsData[] = await getVideoAnalyticsByUser(db, userId);
-        setAnalyticsData(data);
+
+        // Merge analytics data with video details
+        const mergedData = data.map((item) => {
+          const videoDetail = videoDetails.find((video) => video.id == item.video_id.toString()) || {};
+          return { ...item, ...videoDetail };
+        });
+
+        setAnalyticsData(mergedData);
       } catch (error) {
         console.error('Error fetching analytics:', error);
       }
     };
 
     fetchDetails();
+
+    console.log("videoDetails:", videoDetails);
+
   }, [db, userId]);
 
-  return (
-    <SafeAreaView style={{ flex: 1 }} className="bg-purple-700 p-4">
-      <View className="flex-1">
-        <Text className="text-white text-2xl font-black text-center mb-4">Analytics Dashboard</Text>
 
-        {/* Table Header */}
-        <View className="flex-row justify-between border-b-2 border-white pb-2">
-          <Text className="text-white font-bold w-1/6 text-center">Video ID</Text>
-          <Text className="text-white font-bold w-1/6 text-center">Lang</Text>
-          <Text className="text-white font-bold w-1/6 text-center">Watched</Text>
-          <Text className="text-white font-bold w-1/6 text-center">Clicks</Text>
-          <Text className="text-white font-bold w-1/6 text-center">Date</Text>
+  const { height } = Dimensions.get("window");
+
+
+  const [open, setOpen] = useState(false);
+  const [sortoption, setSortOption] = useState<string | null>(null);
+  const [items] = useState([
+    { label: "Max Views", value: "max_views" },
+    { label: "Min Views", value: "min_views" },
+    { label: "Max Watched", value: "max_watch_time" },
+    { label: "Min Watched", value: "min_watch_time" },
+  ]);
+
+
+  useEffect(() => {
+    if (!sortoption) return; // If no sorting is selected
+
+    let sortedData = [...analyticsData];
+
+    switch (sortoption) {
+      case "max_views":
+        sortedData.sort((a, b) => b.total_views_day - a.total_views_day);
+        break;
+      case "min_views":
+        sortedData.sort((a, b) => a.total_views_day - b.total_views_day);
+        break;
+      case "max_watch_time":
+        sortedData.sort((a, b) => b.total_time_day - a.total_time_day);
+        break;
+      case "min_watch_time":
+        sortedData.sort((a, b) => a.total_time_day - b.total_time_day);
+        break;
+      default:
+        break;
+    }
+
+    setAnalyticsData(sortedData);
+  }, [sortoption]);
+
+  return (
+    <SafeAreaView style={{ flex: 1, minHeight: height }} className="bg-white p-4">
+      <View className="flex-1">
+        <Text className="text-black text-2xl font-black text-center mb-4">Analytics</Text>
+
+        <TouchableOpacity className="bg-purple-700 my-4 p-3 mt-4 w-full">
+          <Text className="text-white text-center font-bold">EXPORT</Text>
+        </TouchableOpacity> 
+
+        <View className='flex flex-row justify-between'>
+          <Text className='text-2xl font-black'>Videos</Text>
+          <View className='flex flex-row bg-yellow-200 w-[200px]'>
+            <Text className="bg-purple-700 text-white text-sm text-center py-[0.6rem] flex items-center justify-center w-[80px] h-[35px]">
+              Sort By
+            </Text>
+            <DropDownPicker
+              open={open}
+              value={sortoption}
+              items={items}
+              setOpen={setOpen}
+              setValue={(callback) => {
+                const newValue = callback(sortoption);
+                setSortOption(newValue);
+              }}
+              containerStyle={{ maxWidth: 125, alignSelf: "center", marginBottom: 0, paddingHorizontal: 0, padding: 0, paddingTop: 0 }}
+              textStyle={{ fontSize: 12 }}
+              arrowIconStyle={{ marginHorizontal: -5 }}
+              modalAnimationType='slide'
+              // style={{  borderRadius: 0, padding:-20 , margin:0}}
+              placeholder={"Select"}
+              style={{
+                borderWidth: 1,
+                borderColor: '#d5d5d9',
+                backgroundColor: '#ECE6F0',
+                borderRadius: 0,
+                paddingHorizontal: 5,
+                minHeight: 35,
+              }}
+              tickIconContainerStyle={{ backgroundColor: "red" }}
+              dropDownContainerStyle={{
+                backgroundColor: '#ECE6F0',
+                borderWidth: 1,
+                borderColor: '#d5d5d9',
+                borderRadius: 0,
+                gap: 10
+              }}
+            />
+          </View>
         </View>
 
-        {/* Table Data */}
         <FlatList
           data={analyticsData}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => (
             <View className="flex-row justify-between border-b border-white py-2">
-              <Text className="text-white w-1/6 text-center">{item.video_id}</Text>
-              <Text className="text-white w-1/6 text-center">{item.language}</Text>
-              <Text className="text-white w-1/6 text-center">{item.total_time_day} min</Text>
-              <Text className="text-white w-1/6 text-center">{item.total_views_day}</Text>
-              <Text className="text-white w-1/6 text-center">{item.date}</Text>
+
+
+              <View className="flex flex-row items-center justify-between border-b-[1px] border-gray-300 h-fit min-h-[130px]">
+
+                <Image
+                  source={item.language === "en" ? item.thumbnail_en : item.thumbnail_punjabi}
+                  style={{ height: 100, width: "50%" }}
+                  resizeMode="contain"
+                  className='w-1/2'
+                />
+
+                {/* Video Details */}
+                <View className="flex w-1/2 ml-2 justify-between items-start gap-0 min-h-[100px]">
+                  <Text className="text-left text-lg w-full font-bold break-words">
+                    {item.language === "en" ? item.english_title : item.punjabi_title}
+                  </Text>
+                  <View className='flex gap-0'>
+                    <Text className="text-base font-bold text-purple-700">
+                      Watch Time: {item.total_time_day} s
+                    </Text>
+
+                    <Text className="text-base font-bold text-purple-700">
+                      Total Views: {item.total_views_day}
+                    </Text>
+
+                    <Text className="text-base font-bold text-purple-700">
+                      Watched in: {item.language === "en" ? "English" : "Punjabi"}
+                    </Text>
+                  </View>
+                </View>
+              </View>
             </View>
           )}
         />
@@ -84,5 +204,7 @@ const AnalyticsDashboard = () => {
     </SafeAreaView>
   );
 };
+
+
 
 export default AnalyticsDashboard;
