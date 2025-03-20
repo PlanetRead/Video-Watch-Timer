ecdf2e9d9f89441763cf56f11cd4912d48395d89
Jiya Gupta <coderaholic21@gmail.com>
2025-03-20 06:05:34 +0530

    Added more details to supabase db

--- components/SyncToCloud.tsx
+++ components/SyncToCloud.tsx
@@ -3,23 +3,27 @@ import React from "react";
 import { supabase } from "@/utils/SupabaseConfig";
 import { getUsers, getVideoAnalyticsByUser } from "@/app/database/database";
 import { useSQLiteContext } from "expo-sqlite";
+import { videoDetails } from "@/assets/details";
 
 interface User {
-    id: string;
-    user_name: string;
-    pin: number;
-    video_analytics?: VideoAnalytics[];
-  }
-  
-  interface VideoAnalytics {
-    id: number;
-    video_id: number;
-    date: string;
-    total_views_day: number;
-    total_time_day: number;
-    last_time_stamp: number | null;
-    language: string;
-  }
+  id: string;
+  user_name: string;
+  pin: number;
+  video_analytics?: VideoAnalytics[];
+}
+
+interface VideoAnalytics {
+  id: number;
+  video_id: number;
+  english_title?: string;
+  punjabi_title?: string;
+  level?: string;
+  date: string;
+  total_views_day: number;
+  total_time_day: number;
+  last_time_stamp: number | null;
+  language: string;
+}
 
 const SyncToCloud = () => {
   const db = useSQLiteContext();
@@ -27,21 +31,31 @@ const SyncToCloud = () => {
     console.log("Syncing to cloud...");
     try {
       const users: User[] = await getUsers(db); // Fetch all users
-        // Fetch video analytics for each user
-        for (const user of users) {
-            const videoAnalytics: VideoAnalytics[] = await getVideoAnalyticsByUser(db, user.id);
-            user.video_analytics = videoAnalytics;
-          }
+      // Fetch video analytics for each user
+      for (const user of users) {
+        const videoAnalytics: VideoAnalytics[] = await getVideoAnalyticsByUser(
+          db,
+          user.id
+        );
+        const mergedAnalytics = videoAnalytics.map((item) => {
+          const videoDetail =
+            videoDetails.find((video) => {
+              return video.id === item.video_id.toString();
+            }) || {};
+
+          return { ...item, ...videoDetail };
+        });
+        user.video_analytics = mergedAnalytics;
+      }
       console.log(users);
       await syncUsers(users); // Sync users to the cloud
-        console.log("Synced to cloud");
+      console.log("Synced to cloud");
     } catch (error) {
       console.error("Error fetching user details:", error);
     }
   };
 
-  //@ts-ignore
-  async function syncUsers(users) {
+  async function syncUsers(users : User[]) {
     for (const user of users) {
       // Upsert user
       await supabase.from("user").upsert({
@@ -51,23 +65,34 @@ const SyncToCloud = () => {
       });
 
       // Upsert video analytics
+      if (user.video_analytics?.length) {
       for (const analytics of user.video_analytics) {
         const lastTimestamp = analytics.last_time_stamp
           ? new Date(analytics.last_time_stamp).getTime()
           : null;
-    
-        const { data, error } = await supabase.from("video_analytics").upsert({
-          user_id: user.id,
-          video_id: analytics.video_id,
-          date: analytics.date,
-          total_views_day: analytics.total_views_day,
-          total_time_day: analytics.total_time_day,
-          last_time_stamp: lastTimestamp,
-          language: analytics.language,
-        }).select();
 
-        console.log(data,error);
+        const { data, error } = await supabase
+          .from("video_analytics")
+          .upsert(
+            {
+              user_id: user.id,
+              video_id: analytics.video_id,
+              english_title: analytics.english_title,
+              punjabi_title: analytics.punjabi_title,
+              level: analytics.level,
+              date: analytics.date,
+              total_views_day: analytics.total_views_day,
+              total_time_day: analytics.total_time_day,
+              last_time_stamp: lastTimestamp,
+              language: analytics.language,
+            },
+            { onConflict: ["user_id", "video_id"] }
+          ) // Ensure unique combo of user_id & video_id
+          .select();
+
+        console.log(data, error);
       }
+    }
     }
   }
   return (
